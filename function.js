cee790599e58a0bcbccce6d8bcb8259754ba342a
tiendat259 <ngotiendat259@gmail.com>
2022-05-05 20:58:27 +0700

    update source code

--- function.js
+++ function.js
@@ -106,10 +106,12 @@ function checkinfo() {
     var lenght = document.getElementById('txtlenght').value;
     var width = document.getElementById('txtwidth').value;
     if (name == "" && phonenum == "" && add == "") {
-        window.alert("Vui lòng nhập đầy đủ thông tin để nhận báo giá chi tiết!")
+        window.alert("Vui lòng nhập đầy đủ thông tin để nhận báo giá chi tiết!");
+        window.location = "../html/cart.html";
     } else {
         var result = lenght * width;
         document.getElementById("tongtien").innerHTML = result;
+
     }
 }
 
@@ -121,133 +123,4 @@ function datatransfer() {
     y.style.display = "block"
     var alu = document.getElementById('select-alu').value;
     document.getElementById('type').innerHTML = alu;
-}
-//Cart
-let cartIcon = document.querySelector("#cart-icon");
-let cart = document.querySelector(".cart");
-let closecart = document.querySelector("#close-cart");
-//Open Cart
-cartIcon.onclick = () => {
-    cart.classList.add("active");
-};
-//Close Cart
-closecart.onclick = () => {
-    cart.classList.remove("active");
-};
-
-//Cart Working JS
-if (document.readyState == "loading") {
-    document.addEventListener("DOMContentLoaded", ready);
-} else {
-    ready();
-}
-
-//Making Function
-function ready() {
-    //Remove Items From Cart
-    var removeCartButtons = document.getElementsByClassName("cart-remove");
-    console.log(removeCartButtons);
-    for (var i = 0; i < removeCartButtons.length; i++) {
-        var button = removeCartButtons[i];
-        button.addEventListener("click", removeCartItem);
-    }
-    //Quantity Changes
-    var quantityInputs = document.getElementsByClassName("cart-quantity");
-    for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i];
-        input.addEventListener("change", quantityChanged);
-    }
-}
-
-//Add to Cart
-var addCart = document.getElementsByClassName('add-cart');
-for (var i = 0; i < addCart.length; i++) {
-    var button = addCart[i];
-    button.addEventListener("click", addCartClicked);
-
-    //Buy Button Work
-    document.getElementsByClassName('btn-buy')[0].addEventListener("click", buyButtonClicked);
-}
-
-//Buy Button
-function buyButtonClicked() {
-    alert('Đơn hàng của bạn đã được gửi!');
-    var cartContent = document.getElementsByClassName('cart-content')[0];
-    if (cartContent == "") {
-        alert('Đơn hàng trống!')
-    }
-    while (cartContent.hasChildNodes()) {
-        cartContent.removeChild(cartContent.firstChild);
-    }
-
-    updatetotal();
-
-}
-//Remove Items From Cart
-function removeCartItem(event) {
-    var buttonClicked = event.target;
-    buttonClicked.parentElement.remove();
-    updatetotal();
-}
-//Quantity Changes
-function quantityChanged(event) {
-    var input = event.target;
-    if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1;
-    }
-    updatetotal();
-}
-
-//Add to Cart
-function addCartClicked(event) {
-    var button = event.target;
-    var shopProducts = button.parentElement;
-    var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
-    var price = shopProducts.getElementsByClassName("price")[0].innerText;
-    var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
-    addProductToCart(title, price, productImg);
-    updatetotal();
-}
-
-function addProductToCart(title, price, productImg) {
-    var cartShopBox = document.createElement("div");
-    cartShopBox.classList.add('cart-box');
-    var cartItems = document.getElementsByClassName("cart-content")[0];
-    var cartItemsNames = document.getElementsByClassName("cart-product-title");
-    for (var i = 0; i < cartItemsNames.length; i++) {
-        if (cartItemsNames[i].innerText == title) {
-            alert("Sản phẩm đã tồn tại trong giỏ hàng!");
-            return;
-        }
-    }
-    var cartBoxContent = `
-<img src="${productImg}" class="cart-img">
-<div class="detail-box">
-    <div class="cart-product-title">${title}</div>
-    <div class="cart-price">${price}</div>
-    <input type="number" value="1" class="cart-quantity" id="">
-</div>
-<i class="fa fa-trash cart-remove"></i>
-`;
-    cartShopBox.innerHTML = cartBoxContent;
-    cartItems.append(cartShopBox);
-    cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener("click", removeCartItem);
-    cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener("change", quantityChanged);
-}
-
-//Update Total
-function updatetotal() {
-    var cartContent = document.getElementsByClassName("cart-content")[0];
-    var cartBoxes = cartContent.getElementsByClassName("cart-box");
-    var total = 0;
-    for (var i = 0; i < cartBoxes.length; i++) {
-        var cartBox = cartBoxes[i];
-        var priceElement = cartBox.getElementsByClassName("cart-price")[0];
-        var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
-        var quantity = quantityElement.value;
-        var price = parseFloat(priceElement.innerText.replace("", ""));
-        total = total + (price * quantity) * 1000000;
-    }
-    total = Math.round(total * 100) / 100;
-    document.getElementsByClassName("total-price")[0].innerText = parseFloat(total);
 }
